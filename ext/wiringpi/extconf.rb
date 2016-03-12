6372e3034eaec66c00e8e5b9e70db69b45cb2283
Phil Howard <phil@gadgetoid.com>
2016-03-12 11:46:02 +0000

    Updated extconf to include devLib and find headers

--- ext/wiringpi/extconf.rb
+++ ext/wiringpi/extconf.rb
@@ -1,8 +1,32 @@
 require 'mkmf'
 
+LIBDIR      = RbConfig::CONFIG['libdir']
+INCLUDEDIR  = RbConfig::CONFIG['includedir']
+
 $srcs = Dir.glob('WiringPi/wiringPi/*.c')
-$objs = $srcs.map{ |file| file.sub('.c','.o') }
+$srcs += Dir.glob('WiringPi/devLib/*.c')
 $srcs << 'wiringpi_wrap.c'
-$objs << 'wiringpi_wrap.o'
+
+$srcs -= ['WiringPi/devLib/piFaceOld.c']
+
+$objs = $srcs.map{ |file| file.sub('.c','.o') }
+
+HEADER_DIRS = [
+  './WiringPi/wiringPi',
+  './WiringPi/devLib',
+  '/usr/local/include',
+  INCLUDEDIR,
+  '/usr/include',
+]
+
+LIB_DIRS = [
+  './WiringPi/wiringPi',
+  './WiringPi/devLib',
+  '/usr/local/lib',
+  LIBDIR,
+  '/usr/lib',
+]
+
+dir_config('wiringpi', HEADER_DIRS, LIB_DIRS)
 
 create_makefile('wiringpi/wiringpi')    
